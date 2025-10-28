81bb45f7075fb81b58f1a3ca428ceca0f2a71e33
Lara-RE301 <larinhica@gmail.com>
2025-10-28 19:19:11 +0100

    trying to fix something that it isnt working in gitbash

--- ZAD2/ZAD2/kod_zd2.c
+++ ZAD2/ZAD2/kod_zd2.c
@@ -1,5 +1,5 @@
-/*Definirati strukturu osoba(ime, prezime, godina roðenja) i napisati program koji : A.dinamièki dodaje novi element na poèetak liste,
-B.ispisuje listu,C.dinamièki dodaje novi element na kraj liste,D.pronalazi element u listi(po prezimenu),E.briše odreðeni element iz liste,
+ï»¿/*Definirati strukturu osoba(ime, prezime, godina roÃ°enja) i napisati program koji : A.dinamicki dodaje novi element na pocetak liste,
+B.ispisuje listu,C.dinamicki dodaje novi element na kraj liste,D.pronalazi element u listi(po prezimenu),E.briÅ¡e odredeni element iz liste,
 U zadatku se ne smiju koristiti globalne varijable.*/
 
 #define _CRT_SECURE_NO_WARNINGS
@@ -8,7 +8,7 @@ U zadatku se ne smiju koristiti globalne varijable.*/
 #include <string.h>
 
 typedef struct Person* Position; //pokazivac na strukturu
-typedef struct Person { 
+typedef struct Person {
 	char name[50];
 	char last_name[50];
 	int birth_year;
@@ -16,19 +16,19 @@ typedef struct Person {
 }Person;
 
 int addTotheBeginning(Position); //deklaracija svi funkcije koje su u ovom kodu
-int Printlist(Position); 
-int addTotheEnd(Position); 
-Position FindlastName(char* lastName, Position); 
-Position FindPreviouslastName(char* lastName, Position); 
-int removeFromtheList(char* lastName, Position); 
+int Printlist(Position);
+int addTotheEnd(Position);
+Position FindlastName(char* lastName, Position);
+Position FindPreviouslastName(char* lastName, Position);
+int removeFromtheList(char* lastName, Position);
 int DeleteList(Position);
 
 int main() {
 	Person Head = { .name = "", .last_name = "", .birth_year = 0, .Next = NULL }; //postavljanje prvi element HEAD na "nulu", koji se nece ispisati
 	char lastName[50];
 	int temp = 0;
-	Position personFound=NULL;
-	
+	Position personFound = NULL;
+
 	addTotheBeginning(&Head); //pozivanje funkcija unos prije prethodni element
 	printf("\n Lista nakon unosa na pocetku:\n");
 	Printlist(Head.Next); //pozivanje funkcija za ispis
@@ -38,7 +38,7 @@ int main() {
 	Printlist(Head.Next); //za ispis svi elementi
 
 	printf("\n Zelite li pronaci odredenu osobu? Ako da, unesite prezime. Ako ne, unesite NE: "); // if/else uvjeti da korisnik moze odluciti ako zeli ili ne pronaci osobu iz liste.
-	scanf("%s", lastName);																		
+	scanf("%s", lastName);
 	if (strcmp(lastName, "NE") == 0) {															//ako odgovori ne, pojavi se poruku da nije htio,
 		printf("\n Ne zelite nitko pronaci!");
 	}
@@ -50,7 +50,7 @@ int main() {
 		else
 			printf("\n Prezime koje ste unjeli, %s, se nalazi u listi na adresu %p", lastName, personFound);
 	}
-		
+
 	printf("\n Zelite li izbrisati netko iz liste? Ako da, unesite prezime. Ako ne, unesite NE: "); // if/else uvjeti ako korinik zeli izbrisati osobu iz liste
 	scanf("%s", lastName);
 	if (strcmp(lastName, "NE") == 0) {																//ako odgovor je ne, samo se pojavi poruka da nije htio
@@ -72,12 +72,12 @@ int main() {
 
 int addTotheBeginning(Position p) {
 
-	int N=0, i;
+	int N = 0, i;
 	printf("\n Koliko ljudi zelite unjeti u listi? ");
 	scanf("%d", &N);
 
 	for (i = 0; i < N; i++) {  //petlja za unos u listi
-	//potrebno je da alociranje memoriju za q bude unutra petlje jer za svaku novu osobu stvorit ce se novi prostor u memoriji
+		//potrebno je da alociranje memoriju za q bude unutra petlje jer za svaku novu osobu stvorit ce se novi prostor u memoriji
 		Position q;
 		q = (Position)malloc(sizeof(Person));
 		if (q == NULL) {
@@ -90,7 +90,7 @@ int addTotheBeginning(Position p) {
 		scanf("%d", &q->birth_year);
 
 		q->Next = p->Next; //povezivanje elementi iz liste korisceni pokazivaci
-		p->Next = q;	
+		p->Next = q;
 	}
 	return 0;
 }
@@ -107,16 +107,16 @@ int Printlist(Position p) {
 }
 
 int addTotheEnd(Position p) {
-		while (p->Next != NULL) { //petlja da se doðe do zadnjeg element listi
+	while (p->Next != NULL) { //petlja da se doÃ°e do zadnjeg element listi
 		p = p->Next;
-		}
-
-	addTotheBeginning(p); //pozivan funkciju addTotheBeginning poslajuæi zadnji element u listi
-	return 0;
 	}
 
+	addTotheBeginning(p); //pozivan funkciju addTotheBeginning poslajuÃ¦i zadnji element u listi
+	return 0;
+}
+
 Position FindlastName(char* lastName, Position p) {
-	
+
 	while (p != NULL && strcmp(p->last_name, lastName) != 0) {	//while petlja se vrti sve dok je p=NULL ili ako je se pronade poslan prezime
 		p = p->Next;
 	}
@@ -137,18 +137,18 @@ int removeFromtheList(char* lastName, Position p) {
 
 	Position temp = Previous->Next; //povezivanje prethodni element s element koji je nakon onaj koji zelimo izbrisati
 	Previous->Next = temp->Next;
+	temp->Next = NULL;
 	free(temp);
 	return 1;
 }
 
 int DeleteList(Position p) {
-	Position Current = p;
 	Position temp = 0;
-
-	while (Current != NULL) {
-		temp = Current->Next;
-		free(Current);
-		Current = temp;
+	while (p->Next != NULL) { //petlja za isbrisanje listu, tj. oslobodi se memoriju u mallocu
+		temp = p->Next;
+		p->Next = temp->Next;
+		temp->Next = NULL;
+		free(temp);
 	}
 	return 0;
 }
